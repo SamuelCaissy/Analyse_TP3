a8e96977d2f4b90388a4924ca6a6ef51071d87eb
Samuel <2045396@cegepmontpetit.ca>
2024-11-21 12:55:31 -0500

    INIT : dossiers initiaux

---
+++ eShopOnWeb-main/src/Infrastructure/Data/CatalogContextSeed.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.eShopWeb.ApplicationCore.Entities;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.eShopWeb.Infrastructure.Data;
+
+public class CatalogContextSeed
+{
+    public static async Task SeedAsync(CatalogContext catalogContext,
+        ILogger logger,
+        int retry = 0)
+    {
+        var retryForAvailability = retry;
+        try
+        {
+            if (catalogContext.Database.IsSqlServer())
+            {
+                catalogContext.Database.Migrate();
+            }
+
+            if (!await catalogContext.CatalogBrands.AnyAsync())
+            {
+                await catalogContext.CatalogBrands.AddRangeAsync(
+                    GetPreconfiguredCatalogBrands());
+
+                await catalogContext.SaveChangesAsync();
+            }
+
+            if (!await catalogContext.CatalogTypes.AnyAsync())
+            {
+                await catalogContext.CatalogTypes.AddRangeAsync(
+                    GetPreconfiguredCatalogTypes());
+
+                await catalogContext.SaveChangesAsync();
+            }
+
+            if (!await catalogContext.CatalogItems.AnyAsync())
+            {
+                await catalogContext.CatalogItems.AddRangeAsync(
+                    GetPreconfiguredItems());
+
+                await catalogContext.SaveChangesAsync();
+            }
+        }
+        catch (Exception ex)
+        {
+            if (retryForAvailability >= 10) throw;
+
+            retryForAvailability++;
+            
+            logger.LogError(ex.Message);
+            await SeedAsync(catalogContext, logger, retryForAvailability);
+            throw;
+        }
+    }
+
+    static IEnumerable<CatalogBrand> GetPreconfiguredCatalogBrands()
+    {
+        return new List<CatalogBrand>
+            {
+                new("Azure"),
+                new(".NET"),
+                new("Visual Studio"),
+                new("SQL Server"),
+                new("Other")
+            };
+    }
+
+    static IEnumerable<CatalogType> GetPreconfiguredCatalogTypes()
+    {
+        return new List<CatalogType>
+            {
+                new("Proteine"),
+                new("Créatine"),
+                new("Accessoire"),
+                new("Vêtements")
+            };
+    }
+
+    static IEnumerable<CatalogItem> GetPreconfiguredItems()
+    {
+        return new List<CatalogItem>
+            {
+                new(2,2, ".NET Bot Black Sweatshirt", ".NET Bot Black Sweatshirt", 19.5M,  "http://catalogbaseurltobereplaced/images/products/Chernobyl.png"),
+                new(1,2, ".NET Black & White Mug", ".NET Black & White Mug", 8.50M, "http://catalogbaseurltobereplaced/images/products/Farheneit.png"),
+                new(2,5, "Prism White T-Shirt", "Prism White T-Shirt", 12,  "http://catalogbaseurltobereplaced/images/products/Alpha.png"),
+                new(2,2, ".NET Foundation Sweatshirt", ".NET Foundation Sweatshirt", 12, "http://catalogbaseurltobereplaced/images/products/HollowPurple.png"),
+                new(3,5, "Roslyn Red Sheet", "Roslyn Red Sheet", 8.5M, "http://catalogbaseurltobereplaced/images/products/Neo.png"),
+                new(2,2, ".NET Blue Sweatshirt", ".NET Blue Sweatshirt", 12, "http://catalogbaseurltobereplaced/images/products/Samurai.png"),
+                new(2,5, "Roslyn Red T-Shirt", "Roslyn Red T-Shirt",  12, "http://catalogbaseurltobereplaced/images/products/Thunderbolt.png"),
+                new(2,5, "Kudu Purple Sweatshirt", "Kudu Purple Sweatshirt", 8.5M, "http://catalogbaseurltobereplaced/images/products/Trident.png"),
+                new(1,5, "Cup<T> White Mug", "Cup<T> White Mug", 12, "http://catalogbaseurltobereplaced/images/products/Yakuza.png"),
+            };
+    }
+}
